b82373271500c5bd938e826fd7ecc09e54c372ce
Abi <abixalmon@users.noreply.github.com>
2020-07-16 17:37:27 +0600

    Update Kernel.php

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,6 +24,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
+        $schedule->command('update:analytics')->everyMinute();
         $schedule->command('auth:clear-resets')->hourly();
     }
 
